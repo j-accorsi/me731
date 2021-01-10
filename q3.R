86b4400939b5aafe722810e9bbb7e76c4c9d1c45
felipepetri <fapsocial18@gmail.com>
2021-01-10 17:55:15 -0300

    perfis de linha e coluna

--- q3.R
+++ q3.R
@@ -11,3 +11,17 @@ saudemental<-saudemental %>%
 chisq.test(saudemental[,-1])
 
 # Tem dependência entre as variáveis
+
+perfil_linha<-saudemental %>% 
+  gather(status,qtd,-1) %>% 
+  group_by(saude) %>% 
+  mutate(qtd=qtd/sum(qtd)) %>% 
+  spread(status,qtd)
+  
+perfil_linha
+
+perfil_coluna<-saudemental %>% 
+  gather(status,qtd,-1) %>% 
+  group_by(status) %>% 
+  mutate(qtd=qtd/sum(qtd)) %>% 
+  spread(status,qtd)
