0d6ec6683b0bd93134c59b37731018ca4e507273
j-accorsi <jsaccorsi@gmail.com>
2021-01-16 11:18:02 -0300

    ajustes nos graficos

--- q2.R
+++ q2.R
@@ -80,9 +80,9 @@ UM = c('Tempo (s)', 'Altura (m)', 'Distância (m)', 'Tempo (s)',
 # Histograma
 par(mfrow=c(2,4),mar=c(4,4,2,2))
 for(i in 1:8){
-  hist(as.numeric(unlist(heptathlon[i])),probability=TRUE, col="#EE6060",
+  hist(as.numeric(unlist(heptathlon[i])),probability=TRUE, col="#80009A",
        main=colnames(cormat)[i],xlab="",ylab=UM[i],
-       cex.axis=1.5, cex.main=1.3)
+       cex.axis=1.5, cex.main=1.3, cex.lab = 1.3)
 }
 
 
@@ -90,7 +90,8 @@ for(i in 1:8){
 par(mfrow=c(2,4),mar=c(4,4,2,2))
 for(i in 1:8){
   boxplot(as.numeric(unlist(heptathlon[i])),probability=TRUE,
-       main=nomes[i],xlab="",ylab=UM[i])
+       main=nomes[i],xlab="",ylab=UM[i],
+       cex.axis=1.5, cex.main=1.75, cex.lab = 1.3)
 }
 
 # Gráfico Q-Q
@@ -105,10 +106,12 @@ for(i in 1:8){
 ############################## Analise Inferencial #############################
 ################################################################################
 
+# Auto valores
 m_cor <- cor(heptathlon)
 aut_val <- eigen(m_cor)$values
 round(aut_val,2)
 
+# Auto vetores
 aut_vec <- -(eigen(m_cor)$vectors)
 round(aut_vec,2)
 
@@ -116,51 +119,60 @@ p <- 8 # numero de variaveis
 
 colnames(heptathlon) <- nomes
 
-m_aut_val < -t(matrix(((aut_val)),p,p))
+m_aut_val <- t(matrix(((aut_val)),p,p))
 result_cp_cor <- princomp(heptathlon,cor=TRUE)
 corr_cp_var <- aut_vec*sqrt(m_aut_val)
 summary(result_cp_cor)
 
+# Grafico da variancia dos componentes
 par(mfrow=c(1,1))
-screeplot(result_cp_cor, type = c("lines"), main = "autovalores",
+screeplot(result_cp_cor, type = c("lines"), main = "", 
           cex = 1.2, cex.lab = 1.2, cex.main = 1.2, pch = 19)
 
-fviz_eig(result_cp_cor, addlabels = TRUE)
+# Grafico do PV
+fviz_eig(result_cp_cor, addlabels = TRUE, xlab = "Componentes", main = "",
+         ylab = "Percentual de variancia explicada", ggtheme = theme_bw(base_size = 16))
 
 # primeiros autovetores
-mcps <- t(round(aut_vec[,1:3],2))
+mcps <- t(round(aut_vec[,1:2],2))
 colnames(mcps) <- nomes
 mcps
 
 # correlações dos dois primeiros autovetores com as variáveis originais
-mcorcps <- t(round(corr_cp_var[,1:3],2))
+mcorcps <- t(round(corr_cp_var[,1:2],2))
 colnames(mcorcps) <- nomes
 mcorcps
 
 cp1 <- -cbind((result_cp_cor$scores)[,1])
 cp2 <- -cbind((result_cp_cor$scores)[,2])
-cp3 <- -cbind((result_cp_cor$scores)[,3])
-boxplot(cbind(cp1,cp2,cp3), cex.lab = 1.2, xlab = "CP")
+boxplot(cbind(cp1,cp2), cex.lab = 1.2, xlab = "CP")
 
-par(mfrow=c(2,2))
+par(mfrow=c(1,2))
 hist(cp1, probability = TRUE, xlab = "CP1", ylab = "Densidade", main = "")
 hist(cp2, probability = TRUE, xlab = "CP2", ylab = "Densidade", main = "")
-hist(cp3, probability = TRUE, xlab = "CP3", ylab = "Densidade", main = "")
 
 # Dispersão entre as componentes
-par(mfrow=c(2,2))
+par(mfrow=c(1,1))
 plot(cp1, cp2, cex = 1.2, pch = 19)
-plot(cp1, cp3, cex = 1.2, pch = 19)
-plot(cp2, cp3, cex = 1.2, pch = 19)
 
+# Biplot padrão
 par(mfrow=c(1,1))
 biplot(result_cp_cor,cex=1)
 
+# Biplot factoextra
 fviz_pca_biplot(result_cp_cor)
 
+# Biplot sem nome
 fviz_pca_biplot(result_cp_cor, 
                 geom.ind = "point",
                 pointshape = 19,
                 pointsize = 1.5,
                 col.ind = "black",
                 repel = TRUE) 
+
+# Rank
+as_tibble(matrix(c(rownames(cp1), round(cp1,2)),
+                 ncol = 2)) %>% 
+  mutate(V2 = as.numeric(V2)) %>%
+  arrange(desc(V2)) %>%
+  view()
